d848b6c886146d1a523d049ff21d73c52cdb031f
Junshuo Zhang <junshuo.zhang@ucdconnect.ie>
2020-04-07 20:40:25 +0800

    新的文件夹结构方便管理静态文件，以后请将文件放入那些目录中

--- main.py
+++ main.py
@@ -2,23 +2,23 @@ from flask import Flask,render_template
 #To run this app, set environment variable "FLASK_APP=main.py", FLASK_ENV="development" is optional, allowing debug features
 #use python -m flask run to run this app,--host=0.0.0.0 will set the website to public
 #dynamic editing is supported, which means you can chagne the code while the website is still running
-app = Flask(__name__,static_url_path='',static_folder='htmls')
+app = Flask(__name__,static_url_path='',template_folder='htmls')
 #static folder means the folder for static html pages
 #route() method binds functions to the corresponded URLs
 #e.g. /url means the function will preform when user visits yourwebsiteurl/url
 @app.route('/')
 def main_page():
-    return app.send_static_file('main_page.html')
+    return render_template('main_page.html')
 @app.route('/register')
 def register():
-    return app.send_static_file('register.html')
+    return render_template('register.html')
 @app.route('/login')
 def login():
-    return app.send_static_file('login.html')
+    return render_template('login.html')
 @app.route('/favorites')
 def favors():
-    return app.send_static_file('favorites.html')
-@app.route('/search')
+    return render_template('favorites.html')
+@app.route('/search',methods=('GET', 'POST'))
 def searchtest():
-    #图片和css无法显示
-    return app.send_static_file('search_web_page.html')
+    file="/images/test.jpg"
+    return render_template('search_web_page.html',file=file)
