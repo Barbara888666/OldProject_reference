05fc8e5c43d6d8e647dab543d0981b16fd0757ca
inknon <jinghan.teng@ucdconnect.ie>
2020-04-12 16:45:17 +0800

    Signed-off-by: inknon <jinghan.teng@ucdconnect.ie>

--- htmls/static/js/searchWebPage.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function () {
-    $(".menu_main>a").click(function () {
-        var Node=$(this).next("ul");
-        Node.toggle(500);
-    })
-
-})
--- main.py
+++ main.py
@@ -13,15 +13,36 @@ def main_page():
 @app.route('/register')
 def register():
     return render_template('register.html')
-@app.route('/login/')
+@app.route('/login')
 def login():
     return render_template('login.html')
 @app.route('/favorites')
 def favors():
     return render_template('favorites.html')
+@app.route('/seller')
+def seller():
+    return render_template('seller_information.html')
+@app.route('/popular')
+def popular():
+    return render_template('popular_products.html')
+@app.route('/news')
+def news():
+    return render_template('my_news.html')
+@app.route('/highpre')
+def highpre():
+    return render_template('high_predibility_seller.html')
+@app.route('/detail')
+def detail():
+    return render_template('商品详情.html')
+@app.route('/notice')
+def notice():
+    return render_template('website_notice.html')
 @app.route('/search',methods=('GET', 'POST'))
 def searchtest():
     #由后端传送文件的示范
     file="/images/test.jpg"
     return render_template('search_web_page.html',file=file)
 
+if __name__=="__main__":
+    app.environment="development"
+    app.run()
