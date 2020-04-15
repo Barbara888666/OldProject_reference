9a1f275c87f2195370f22f39abb1b657993871dd
Junshuo Zhang <junshuo.zhang@ucdconnect.ie>
2020-04-15 19:11:45 +0800

    增加搜索用户功能

--- db/search.py
+++ db/search.py
@@ -4,3 +4,5 @@ def idcheck(id):
     return 'select id from users where users.id=%d'%(id)
 def searchitem(itemname):
     return 'select * from items where items.name like %'+itemname
+def searchuser(userid):
+    return 'select * from users where users.id=%d'%(userid)
