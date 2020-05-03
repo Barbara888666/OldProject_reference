4796c0b7a09cc3f5aa52ea4951b0a75df570037f
Junshuo Zhang <junshuo.zhang@ucdconnect.ie>
2020-05-03 23:13:58 +0800

    增加返回加入物品的编号的功能

--- db/upload.py
+++ db/upload.py
@@ -11,6 +11,9 @@ def registeraccount(id,name,password,email,phonenum,sex,birthday):
 def uploaditem(name,sellerid,description,category,price,isurgent='false'):
     if not isinstance(sellerid,int):
         sellerid=int(sellerid)
+    t=time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
     q='''INSERT INTO items (item_name,seller_id,description,added_date,is_urgent,view_time,category,price)VALUES 
-    ('%s',%d,'%s','%s','%s',0,'%s',%d);'''%(name,sellerid,description,time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()),isurgent,category,price)
+    ('%s',%d,'%s','%s','%s',0,'%s',%d);'''%(name,sellerid,description,t,isurgent,category,price)
     dbop(q,False)
+    return dbop('''select item_id from items where item_name="%s" and seller_id="%d" and added_date="%s" and category="%s" and price="%d"
+    '''%(name,sellerid,t,category,price),True)[0][0]
