156d9de988adfe37383d730a1084c1fcb2504a1a
Junshuo Zhang <junshuo.zhang@ucdconnect.ie>
2020-05-04 20:23:26 +0800

    增加排序选项

--- db/search.py
+++ db/search.py
@@ -9,7 +9,7 @@ def pwcheck(id,pw):
     return False
 def idcheck(id):
     return dbop('select id from users where users.id=%d'%(id),True)
-def searchitems(itemname,page,*category):
+def searchitems(itemname,page,orderby=None,*category):
     query='''select item_id,item_name,user_name from items natural join users where items.seller_id=users.id and items.category='''+"'"+category[0]+"'"+''' and items.item_name like '''+"'%"+itemname+"%'"+'limit 10 offset '+str(page)
     return dbop(query,True)
 def searchitem(item_id):
