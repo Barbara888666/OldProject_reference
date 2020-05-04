ae1dbef0da63ede1f492cbd9e968c3b6fc90aa54
18206136 <19801355517@163.com>
2020-05-04 20:09:49 +0800

    test but nothing change

--- util/sell.py
+++ util/sell.py
@@ -5,14 +5,19 @@ from util.utils.fileuploads import uploadavatar, uploadalbum, uploaditemimg
 sale=Blueprint('sale',__name__)
 @sale.route('/sell/', methods=['GET', 'POST'])
 def sell_item():
+        userid = None
+        if 'id' in session:
+            userid = session['id']
         if request.method == 'GET':
             if 'id' not in session:
                 return redirect('/login')
             return render_template('sell.html')
         else:
             dic = dict(
+                Name=request.form.get('Name'),
                 Category=request.form.get('Category'),
                 Situation=request.form.get('Situation'),
+                Price=request.form.get('Price'),
                 time=request.form.get('time'),
                 Short_term=request.form.get('short time'),
                 long_time=request.form.get("long time"),
