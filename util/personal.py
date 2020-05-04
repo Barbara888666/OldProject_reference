6615d457242870b0339e1bd0188d7866ee9136a1
inknon <jinghan.teng@ucdconnect.ie>
2020-05-04 21:38:20 +0800

    搜索文件翻页嵌入flask

--- util/personal.py
+++ util/personal.py
@@ -1,7 +1,13 @@
-from flask import Blueprint,render_template
+from flask import Blueprint,render_template,request
+from db.search import searchuser
 personal=Blueprint('personal',__name__)
-@personal.route('/personal')
-def personals():
-    return render_template('personal_information.html')
+@personal.route('/personal/<userid>/')
+def personals(userid):
+    username=searchuser(userid=userid)[0][1]
+    email=searchuser(userid=userid)[0][2]
+    phone = searchuser(userid=userid)[0][3]
+
+
+    return render_template('personal_information.html',username=username,email=email,phone=phone)
 #卖家信息：卖家名字，卖家个人介绍，卖家信誉度，卖家qq、微信、电话
 #卖家商品列表：商品图片，商品名字，商品分类
