09259770f8e5f22945f0820d029bb9e38a75f58b
inknon <jinghan.teng@ucdconnect.ie>
2020-05-06 23:00:45 +0800

    搜索文件更改继承，完善继承父模板

--- util/search.py
+++ util/search.py
@@ -8,6 +8,9 @@ search=Blueprint('search',__name__)
 @search.route('/search')
 def searchtest():
     return render_template('search_web_page.html')
+@search.route('/testsearch/')
+def searchs():
+    return render_template('testsearch.html')
 #搜索分类列表及分类物品
 #物品列表：物品id，物品图片，物品名，卖家名字，卖家信誉度，物品介绍信息
 #更换排序（名字，卖家信誉度，买家喜爱度（可取舍该功能）
