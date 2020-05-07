e91ef3a4edae52ed2fd7e8c6b13f8c0add90b153
Junshuo Zhang <junshuo.zhang@ucdconnect.ie>
2020-05-07 23:19:15 +0800

    增加新的数据库内容及配套的搜索函数

--- db/__init__.py
+++ db/__init__.py
@@ -3,7 +3,7 @@ from flask import g
 dbpath=os.path.join(os.path.expanduser('~'),'.market')
 dbfilepath=os.path.join(dbpath,'info.db')
 imgpath=os.path.join(dbpath,'imgs')
-
+#数据库位置：当前用户文件夹下.markets/info.db
 if not os.path.exists(dbpath):
     os.mkdir(dbpath)
 if os.path.isfile(dbfilepath):
@@ -23,29 +23,159 @@ if ('items',) not in tb:
                          NOT NULL,
     description TEXT     NOT NULL,
     added_date  DATETIME NOT NULL,
-    is_urgent   BOOLEAN  DEFAULT (false),
+    is_sell     BOOLEAN  DEFAULT (false) 
+                         NOT NULL,
     view_time   INT      DEFAULT (0) 
                          NOT NULL,
     category    STRING   DEFAULT ('other'),
     price       NUMERIC
-    );
+);
     ''')
     db.commit()
 if ('users',) not in tb:
     db.execute('''
     CREATE TABLE users (
-        id           INTEGER   PRIMARY KEY
+    id           INTEGER   PRIMARY KEY
                            NOT NULL
                            UNIQUE,
-        user_name    TEXT      NOT NULL,
-        password     TEXT      NOT NULL,
-        email        TEXT      NOT NULL,
-        phone_number CHAR (11),
-        sex          BOOLEAN,
-        birth_date   DATE
-    );
+    user_name    TEXT      NOT NULL,
+    password     TEXT      NOT NULL,
+    email        TEXT      NOT NULL,
+    phone_number CHAR (11),
+    sex          BOOLEAN,
+    birth_date   DATE,
+    description  TEXT,
+    avatar_name  TEXT      NOT NULL
+                           DEFAULT ('default_avatar.png') 
+);
+    ''')
+if ('albums',) not in tb:
+    db.execute('''
+    CREATE TABLE albums (
+    owner_id INT  PRIMARY KEY
+                  REFERENCES users (id) ON DELETE CASCADE
+                  NOT NULL,
+    img_name TEXT NOT NULL,
+    seq      INT  NOT NULL
+                  DEFAULT (0) 
+);
     ''')
     db.commit()
+if ('chat_imgs',) not in tb:
+    db.execute('''
+    CREATE TABLE chat_imgs (
+    chat_id  INT  REFERENCES chats (chat_id) ON DELETE CASCADE
+                  NOT NULL
+                  PRIMARY KEY,
+    img_name TEXT NOT NULL,
+    seq      INT  DEFAULT (0) 
+                  NOT NULL
+);
+    ''')
+    db.commit()
+if ('chats',) not in tb:
+    db.execute('''
+    CREATE TABLE chats (
+    chat_id     INTEGER PRIMARY KEY AUTOINCREMENT
+                        UNIQUE
+                        NOT NULL,
+    sender_id   INT     REFERENCES users (id) 
+                        NOT NULL,
+    receiver_id INT     REFERENCES users (user_name) 
+                        NOT NULL,
+    content     TEXT    NOT NULL
+);
+    ''')
+    db.commit()
+if ('item_imgs') not in tb:
+    db.execute('''
+    CREATE TABLE item_imgs (
+    item_id  INT  PRIMARY KEY
+                  REFERENCES items (item_id) ON DELETE CASCADE
+                  NOT NULL,
+    img_name TEXT NOT NULL,
+    seq      INT  NOT NULL
+                  DEFAULT (0) 
+);
+    ''')
+    db.commit()
+if ('item_reports',) not in tb:
+    db.execute('''
+    CREATE TABLE item_reports (
+    item_id       TEXT    REFERENCES items (item_id) 
+                          NOT NULL,
+    report_reason TEXT    NOT NULL,
+    report_id     INTEGER PRIMARY KEY AUTOINCREMENT
+                          NOT NULL
+                          UNIQUE
+);
+    ''')
+    db.commit()
+if ('likes',) not in tb:
+    db.execute('''
+    CREATE TABLE likes (
+    self_id   INT PRIMARY KEY
+                  REFERENCES users (id) ON DELETE CASCADE
+                  NOT NULL,
+    target_id INT REFERENCES users (id) ON DELETE CASCADE
+                  NOT NULL
+);
+    ''')
+    db.commit()
+if ('replies',) not in tb:
+    db.execute('''
+    CREATE TABLE replies (
+    item_id       INT     REFERENCES items (item_id) ON DELETE CASCADE
+                          NOT NULL,
+    user_id       INT     REFERENCES users (id) ON DELETE SET DEFAULT
+                          NOT NULL
+                          DEFAULT ('unexisted user'),
+    reply_content TEXT    NOT NULL,
+    reply_id      INTEGER PRIMARY KEY AUTOINCREMENT
+                          NOT NULL
+                          UNIQUE
+);
+
+    ''')
+    db.commit()
+if ('reply_imgs',) not in tb:
+    db.execute('''
+CREATE TABLE reply_imgs (
+    reply_id INT  PRIMARY KEY
+                  REFERENCES replies (reply_id) ON DELETE CASCADE
+                  NOT NULL,
+    img_name TEXT NOT NULL,
+    seq      INT  NOT NULL
+                  DEFAULT (0) 
+);    
+    ''')
+    db.commit()
+if ('reply_reports',) not in tb:
+    db.execute('''
+    CREATE TABLE reply_reports (
+    report_id     INTEGER PRIMARY KEY AUTOINCREMENT
+                          NOT NULL
+                          UNIQUE,
+    target_reply  INT     REFERENCES replies (reply_content) 
+                          NOT NULL,
+    report_reason TEXT    NOT NULL
+);
+  
+    ''')
+    db.commit()
+if ('user_reports',) not in tb:
+    db.execute('''
+    CREATE TABLE user_reports (
+    report_id     INTEGER PRIMARY KEY AUTOINCREMENT
+                          NOT NULL,
+    report_reason TEXT    NOT NULL,
+    target_id     INT     NOT NULL
+                          REFERENCES users (id) 
+);
+
+    ''')
+    db.commit()
+
 db.close()
 def get_db():
     db = getattr(g, '_database', None)
--- db/search.py
+++ db/search.py
@@ -7,13 +7,31 @@ def pwcheck(id,pw):
     return t.hexdigest() == dbop('select password from users where users.id=%d'%(id),True)[0][0]
 def idcheck(id):
     return dbop('select id from users where users.id=%d'%(id),True)
-def searchitems(itemname,page,orderby=None,*category):
-    query='''select item_id,item_name,user_name from items natural join users where items.seller_id=users.id and items.category='''+"'"+category[0]+"'"+''' and items.item_name like '''+"'%"+itemname+"%'"+'limit 10 offset '+str(page)
+# 输入：可选项：物品名, 类别，页面，排序选项（格式：“{列名称：asc/desc}”,列名称请见名为items的table）
+# 返回带有物品id，物品名称，用户id，物品首个图片的表中表
+def searchitems(itemname:str=None,page:int=None,orderby:dict=None,category='other'):
+    query='''select item_id,item_name,user_name,img_name from items natural join users natural join item_imgs where items.seller_id=users.id  and item_imgs.item_id=items.item_id and item_imgs.seq=0'''
+    if itemname!=None:
+        query=query+'and items.item_name like '''+"'%"+itemname+"%' "
+    query=query+'''and items.category='''+"'"+category+"' "
+    if orderby!=None:
+        for k in orderby:
+            query=query+"order by "+k+" "+orderby[k]+" "
+    if page!=None:
+        query=query+'limit 10 offset '+str(page)
     return dbop(query,True)
+#输入物品id，返回物品的所有信息
 def searchitem(item_id):
-    return dbop('select * from items where items.item_id='+item_id,True)
+    return dbop('select * from items where items.item_id='+item_id,True)[0]
+
+#输入物品id，返回物品的图片以及图片对应的顺序
+def searchitemimgs(item_id:[str,int]):
+    return dbop('select img_name,seq from item_imgs where item_id='+str(item_id)+';')
+#输入物品id和类别，返回当前结果的个数
 def searchitemnum(itemname,*category):
     query='select count(*) from items where category='+"'"+category[0]+"'"+' and items.item_name like '+"'%"+itemname+"%'"
     return dbop(query,True)[0][0]
+#输入用户id，返回名字邮件电话性别出生日期和头像名称
 def searchuser(userid):
-    return dbop('select user_name,email,phone_number,sex,birth_date from users where users.id=%d'%(userid),True)
+    return dbop('select user_name,email,phone_number,sex,birth_date,avatar_name from users where users.id=%d'%(userid),True)
+#相册的所有图片，每一对用户间的私聊，每对私聊的全部具体内容，回复内容及回复图片，一个用户的所有关注和关注这个用户的人
