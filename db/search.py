c33eda36229e52825ab68eef453801255da7bf44
unknown <zikang.guo@ucdconnect.ie>
2020-05-10 21:39:18 +0800

    添加部分方法

--- db/search.py
+++ db/search.py
@@ -34,4 +34,22 @@ def searchitemnum(itemname,*category):
 #输入用户id，返回名字邮件电话性别出生日期和头像名称
 def searchuser(userid):
     return dbop('select user_name,email,phone_number,sex,birth_date,avatar_name from users where users.id=%d'%(userid),True)
-#相册的所有图片，每一对用户间的私聊，每对私聊的全部具体内容，回复内容及回复图片，一个用户的所有关注和关注这个用户的人
+
+def searchalbums(owner_id):
+    return dbop('select * from albums where albums.owner_id='+owner_id,True)
+#输入用户id，返回该用户相簿中的图片以及图片所对应的顺序
+def whohelikes(user_id):
+    return dbop('select user_name from likes natural join users where likes.target_id=users.id and likes.self_id='+user_id,True)[0]
+#输入用户id，返回所有被该用户关注的用户的名字
+def wholikeshim(user_id):
+    return dbop('select user_name from likes natural join users where likes.self_id=users.id and likes.target_id='+user_id,True)[0]
+#输入用户id，返回所有关注该用户的用户的名字
+def searchreplies(reply_id):
+    return dbop('select item_name,item_id,user_name,user_id,reply_content,added_date from replies natural join users natural join items where items.item_id=replies.item_id and users.id=replies.user_id and replies.reply_id='+reply_id,True)[0]
+#输入回复id，返回该用户名字与id，所回复项目的名字与id,以及添加该回复的日期
+def searchreply_imgs(reply_id):
+    return dbop('select * from reply_imgs where reply_imgs.reply_id='+reply_id,True)
+#输入回复id,返回该回复中的图片以及图片所对于的顺序
+def searchchat(chat_id):
+    return dbop('select * from chat where chat.chat_id='+chat_id,True)[0]
+#输入聊天id，返回该聊天的寄件人和收件人id，以及内容和发送时间
