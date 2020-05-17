2980768431db59854ce52fb3d58040e2f52976d9
Junshuo Zhang <junshuo.zhang@ucdconnect.ie>
2020-05-17 19:20:04 +0800

    删除无用内容

--- util/utils/filesearch.py
+++
@@ -1,9 +0,0 @@
-import os
-from util.utils import rjson,getdir,searchimgs
-
-def searchavatar(id):
-    return searchimgs(str(id),'avatar')
-def searchalbum(id):
-    return searchimgs(str(id),'album')
-def searchitemimg(itemid):
-    return searchimgs(str(itemid),'items')
