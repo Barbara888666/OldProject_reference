10f74e3d3e5d1b8e6e926bb547fa25823b42c777
18206136 <19801355517@163.com>
2020-06-06 15:07:32 +0800

    search

---
+++ static/cms/js/frontuser.js
@@ -0,0 +1,20 @@
+$(function () {
+    $(".warning-user-btn").click(function () {
+        var self = $(this);
+        var tr = self.parent().parent();
+        var user_id = tr.attr("data-id");
+        zlajax.post({
+            'url': '/cms/warnfuser/',
+            'data': {
+                'user_id': user_id
+            },
+            'success': function (data) {
+                if(data['code'] == 200){
+                    alert.alertSuccessToast('操作成功！');
+                }else{
+                    alert.alertInfo(data['message']);
+                }
+            }
+        });
+    });
+});
